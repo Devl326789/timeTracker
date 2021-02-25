ce29b83c9f35921dee9f2ec3000c6ee6105e831a
Devl326789 <Devl987623@gmail.com>
2021-02-24 18:45:47 -0800

    color

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Time Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: Colors.grey,
       ),
       home: SignInPage(),
     );
