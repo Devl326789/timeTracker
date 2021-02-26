bf2306889f44047b96f63070433f2fe71e994db6
Devl326789 <Devl987623@gmail.com>
2021-02-25 18:01:27 -0800

    Using extract Method

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -3,25 +3,42 @@ import 'package:flutter/material.dart';
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var scaffold = Scaffold(
       appBar: AppBar(
         actions: [],
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.amberAccent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.black,
-              child: SizedBox(
-                height: 100,
-              ),
-            )
-          ],
-        ),
+      body: buildContainer(),
+    );
+    return scaffold;
+  }
+
+  Widget buildContainer() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+        ],
       ),
     );
   }
