9556fec5dc841c91f994117f81523af14335bb91
Devl326789 <Devl987623@gmail.com>
2021-03-03 19:41:52 -0800

    customRaisedButton Widget

---
+++ lib/widgets/custom_raised_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  CustomRaisedButton({
+    this.child,
+    this.color,
+    this.borderRadius,
+    this.onPressed,
+  });
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: child,
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
