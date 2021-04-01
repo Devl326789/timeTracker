e6cadb3a17e8e9635178248ef8a782bd577cc5e3
Devl326789 <Devl987623@gmail.com>
2021-03-31 19:03:54 -0700

    Social buttons

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../widgets/custom_raised_button.dart';
+import 'package:timeTracker/widgets/custom_raised_button.dart';
+import './sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -31,18 +32,57 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 8,
+            height: 48,
           ),
           CustomRaisedButton(
-            child: Text(
-              'Sign in with Google',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 15.0,
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset('images/google-logo.png'),
+                Text('Sign in with Google'),
+                Opacity(
+                  opacity: 0,
+                  child: Image.asset('images/google-logo.png'),
+                ),
+              ],
             ),
             color: Colors.white,
-            borderRadius: 4,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SignInButton(
+            text: 'Go Anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
             onPressed: () {},
           ),
         ],
--- lib/widgets/custom_raised_button.dart
+++ lib/widgets/custom_raised_button.dart
@@ -6,23 +6,28 @@ class CustomRaisedButton extends StatelessWidget {
   CustomRaisedButton({
     this.child,
     this.color,
-    this.borderRadius,
+    this.borderRadius: 2.0,
+    this.height = 50.0,
     this.onPressed,
   });
   final Widget child;
   final Color color;
   final double borderRadius;
+  final double height;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: onPressed,
-      child: child,
-      color: color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(borderRadius),
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        onPressed: onPressed,
+        child: child,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
         ),
       ),
     );
