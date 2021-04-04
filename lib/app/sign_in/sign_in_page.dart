19cfaae4436f61a5b4e8d2ede621ce3a0ba01d34
Devl326789 <Devl987623@gmail.com>
2021-04-04 09:13:11 -0700

    Dependency injection for auth

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -2,15 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:timeTracker/app/sign_in/social_signin_button.dart';
 import './sign_in_button.dart';
+import '../../services/auth.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({Key key, @required this.onSignIn}) : super(key: key);
+  const SignInPage({Key key, @required this.auth, @required this.onSignIn})
+      : super(key: key);
+  final AuthBase auth;
   final void Function(User) onSignIn;
 
   Future<void> _signInAnonymously() async {
     try {
-      final userCredentials = await FirebaseAuth.instance.signInAnonymously();
-      onSignIn(userCredentials.user);
+      final user = await auth.signInAnonymously();
+      onSignIn(user);
     } catch (e) {
       print(e.toString());
     }
