64bd6eb457c837834c6dd6d544c2c715d9e6591b
Devl326789 <Devl987623@gmail.com>
2021-04-04 17:34:39 -0700

    refactor signin flow to use the new strreambuilder

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,19 +1,15 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:timeTracker/app/sign_in/social_signin_button.dart';
 import './sign_in_button.dart';
 import '../../services/auth.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({Key key, @required this.auth, @required this.onSignIn})
-      : super(key: key);
+  const SignInPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
-  final void Function(User) onSignIn;
 
   Future<void> _signInAnonymously() async {
     try {
       final user = await auth.signInAnonymously();
-      onSignIn(user);
     } catch (e) {
       print(e.toString());
     }
