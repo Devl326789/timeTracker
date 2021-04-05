5c74d7aab0306f45e421796e4adc800f8dc84572
Devl326789 <Devl987623@gmail.com>
2021-04-05 13:47:20 -0700

    Custom button, email sign in form

--- lib/app/sign_in/email_sigin_form.dart
+++ lib/app/sign_in/email_sigin_form.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:timeTracker/widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          hintText: 'Test@test.com',
+          labelText: 'Email',
+        ),
+      ),
+      SizedBox(height: 8.0),
+      TextField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Password'),
+      ),
+      SizedBox(height: 8.0),
+      FormSubmitButton(
+        text: 'Sign in',
+        onPressed: () {},
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        onPressed: () {},
+        child: Text('Need and account? Register'),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
