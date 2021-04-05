ab27be1185830cfc12b352be153431d90347813f
Devl326789 <Devl987623@gmail.com>
2021-04-05 14:06:52 -0700

    Using TextEditingController

--- lib/app/sign_in/email_sigin_form.dart
+++ lib/app/sign_in/email_sigin_form.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:timeTracker/widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit() {
+    print(
+        "email: ${_emailController.text}, Password: ${_passwordController.text}");
+  }
+
   List<Widget> _buildChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           hintText: 'Test@test.com',
           labelText: 'Email',
@@ -12,17 +21,20 @@ class EmailSignInForm extends StatelessWidget {
       ),
       SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
+        autocorrect: false,
+        keyboardType: TextInputType.visiblePassword,
         obscureText: true,
         decoration: InputDecoration(labelText: 'Password'),
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
         text: 'Sign in',
-        onPressed: () {},
+        onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       TextButton(
-        onPressed: () {},
+        onPressed: _submit,
         child: Text('Need and account? Register'),
       )
     ];
