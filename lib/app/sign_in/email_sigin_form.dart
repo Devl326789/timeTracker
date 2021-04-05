e6e527c7e2e3f918b7b668972471c2c2ac32cf5b
Devl326789 <Devl987623@gmail.com>
2021-04-05 14:19:56 -0700

    Stateful and toggle for signin or register

--- lib/app/sign_in/email_sigin_form.dart
+++ lib/app/sign_in/email_sigin_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:timeTracker/widgets/form_submit_button.dart';
 
+enum EmailSignInFormType { signIn, register }
+
 class EmailSignInForm extends StatefulWidget {
   @override
   _EmailSignInFormState createState() => _EmailSignInFormState();
@@ -10,12 +12,30 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
   void _submit() {
     print(
         "email: ${_emailController.text}, Password: ${_passwordController.text}");
   }
 
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
     return [
       TextField(
         controller: _emailController,
@@ -34,13 +54,13 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        text: 'Sign in',
+        text: primaryText,
         onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       TextButton(
-        onPressed: _submit,
-        child: Text('Need and account? Register'),
+        onPressed: _toggleFormType,
+        child: Text(secondaryText),
       )
     ];
   }
