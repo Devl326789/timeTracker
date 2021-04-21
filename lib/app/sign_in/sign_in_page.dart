b581587d23ee776bc0eeb826f521a4b79ff4b03b
Devl326789 <Devl987623@gmail.com>
2021-04-20 17:58:46 -0700

    Adding consumer

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -9,11 +9,15 @@ import 'package:timeTracker/widgets/show_exception_alert.dart';
 import './sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
+  const SignInPage({Key key, @required this.bloc}) : super(key: key);
+  final SignInBloc bloc;
+
   static Widget create(BuildContext context) {
     return Provider<SignInBloc>(
-      create: (_) => SignInBloc(),
-      child: SignInPage(),
-    );
+        create: (_) => SignInBloc(),
+        child: Consumer<SignInBloc>(
+          builder: (_, bloc, __) => SignInPage(bloc: bloc),
+        ));
   }
 
   void _showSignInError(BuildContext context, Exception exception) {
@@ -29,7 +33,6 @@ class SignInPage extends StatelessWidget {
   }
 
   Future<void> _signInAnonymously(BuildContext context) async {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     try {
       bloc.setIsLoading(true);
       final auth = Provider.of<AuthBase>(context, listen: false);
@@ -51,7 +54,6 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         actions: [],
